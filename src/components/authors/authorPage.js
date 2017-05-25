e7e15f3604b0e8e123ec3f6c21c7ea54cfa8070e
Ander Rodriguez <AnderRodriguezVarela@gmail.com>
2017-05-25 23:54:01 +0200

    Separate author component

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -1,46 +1,31 @@
 "use strict";
 
 var React = require('react');
-var AuthorAPi = require('../../api/authorApi');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
 
-var Author = React.createClass({
+var AuthorPage = React.createClass({
     getInitialState: function () {
         return {
-            author: []
+            authors: []
         };
     },
 
-    componentWillMount: function () {
-        this.setState({authors: AuthorAPi.getAllAuthors()});
+    componentDidMount: function () {
+        if (this.isMounted()) {
+            this.setState({authors: AuthorApi.getAllAuthors()});
+        }
     },
 
     render: function () {
-        var createAuthorRow = function (author) {
-            return (
-                <tr key={author.id}>
-                    <td><a href={"/#authors/" + author.id}>{author.id}</a></td>
-                    <td>{author.firstName} {author.lastName}</td>
-                </tr>
-            );
-        };
-
         return (
             <div>
                 <h1>Authors</h1>
 
-                <table className="table">
-                    <thead>
-                        <th>ID</th>
-                        <th>name</th>
-                    </thead>
-
-                    <tbody>
-                        {this.state.authors.map(createAuthorRow, this)}
-                    </tbody>
-                </table>
+                <AuthorList authors={this.state.authors} />
             </div>
         );
     }
 });
 
-module.exports = Author;
+module.exports = AuthorPage;
