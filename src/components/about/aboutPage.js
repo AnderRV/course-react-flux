5f064ca5f0a62fc6158563feaa0d4507cc61003b
Ander Rodriguez <AnderRodriguezVarela@gmail.com>
2017-05-29 22:52:37 +0200

    Add router

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,6 +3,22 @@
 var React = require('react');
 
 var About = React.createClass({
+    statics: {
+        willTransitionTo: function (transition, params, query, callback) {
+            if (!confirm('sure want to go there?')) {
+                transition.abort();
+            } else {
+                callback();
+            }
+        },
+
+        willTransitionFrom: function (transition, component) {
+            if (!confirm('sure want to leave?')) {
+                transition.abort();
+            }
+        }
+    },
+
     render: function () {
         return (
             <div>
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,6 +1,8 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Home = React.createClass({
     render: function () {
@@ -8,6 +10,8 @@ var Home = React.createClass({
             <div className="jumbotron">
                 <h1>Pluralsight Admin</h1>
                 <p>React, ....</p>
+
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
             </div>
         );
     }
